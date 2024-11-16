0925145329d9de3d10cdd8953422066bf34d4c81
coastcao <coastcao@tencent.com>
2024-11-16 21:32:53 +0800

    add infer_v2

--- launch/cpp/openvino/infer_v2.cpp
+++ launch/cpp/openvino/infer_v2.cpp
@@ -13,236 +13,65 @@
 #include <fstream>
 #include <iostream>
 #include <thread>
+#include <ctime>
+#include <random>
+#include <functional>
+
 
 #include "boost/regex.hpp"
 #include "speak_lib.h"
 #include "openvino/openvino.hpp"
 
-
-#include "../include/cinatra.hpp"
+#include "cinatra.hpp"
 #include "cinatra/metric_conf.hpp"
+#include "utils.hpp"
+
 using namespace cinatra;
 using namespace ylt::metric;
 using namespace std::chrono_literals;
 
-void decode_character(std::map<std::string, int>& char2id_map,
-                    const std::string& input_str,
-                    std::vector<int>& ids) {
-    size_t idx = 0;
-    while (idx < input_str.size()) {
-		if ((input_str[idx] & 0x80) != 0){
-            std::string ch = input_str.substr(idx, 2);
-            ids.push_back(char2id_map[ch]);
-            idx += 2;
-        } else {
-            std::string ch = input_str.substr(idx, 1);
-            ids.push_back(char2id_map[ch]);
-            idx += 1;
-        }
 
-    }      
-}
-
-void  insert_blank(const std::vector<int>& input_ids,
-                 const int& blank_id,
-                 std::vector<int>& output_ids) {
-    output_ids.push_back(blank_id);
-
-    for(auto id : input_ids) {
-        output_ids.push_back(id);
-        output_ids.push_back(blank_id);
-    }                
-}
-
-
-int strip_to_restore(const std::string& text,
-                    std::vector<std::tuple<std::string, std::string>>& puncs,
-                    std::vector<std::string>& sub_texts) {
-    std::cout << "get text:" << text << std::endl;
-    boost::regex reg("[«»,.!]+");
-    boost::sregex_iterator it(text.begin(),text.end(),reg);
-    boost::sregex_iterator begin, end;
-
-    while (it!=end) {
-        std::string punc_label = (it++)->str();
-        std::string position = "MIDDLE";
-        if (text.substr(0, punc_label.size()) == punc_label)
-            position = "BEGIN";
-        else if (text.substr(text.size()-punc_label.size(), text.size()) == punc_label) {
-            position = "END";
-        }
-            
-        puncs.push_back(std::tuple<std::string,std::string>(punc_label, position));
-    }
-
-    boost::sregex_token_iterator tit(text.begin(),text.end(),reg,-1);
-    boost::sregex_token_iterator tend;
-    while (tit!=tend) {
-        std::string t = *tit++;
-        if (t != "")
-            sub_texts.push_back(t);
-    }
-}
-
-
-int restore(std::vector<std::tuple<std::string, std::string>>& puncs,
-            std::vector<std::string>& sub_texts,
-            std::vector<std::string>& text) {
-    if (puncs.size() <= 0) {
-        std::string text_s = "";
-        for (auto s : sub_texts) {
-            text_s += s;
-        }
-        text.push_back(text_s);
-        std::cout << text[0] << std::endl;
-    } else if (sub_texts.size() <= 0) {
-        std::string punc_str = "";
-        for (auto p : puncs) {
-            punc_str += std::get<0>(p);
-        }
-        text.push_back(punc_str);
-        std::cout << text[0] << std::endl;
-    } else {
-
-        std::tuple<std::string, std::string> current_punc = puncs[0];
-        if (std::get<1>(current_punc) == "BEGIN") {
-            std::string prefix_str = std::get<0>(current_punc) + sub_texts[0];
-            puncs.erase(puncs.begin());
-            sub_texts[0] = prefix_str;
-            restore(puncs, sub_texts, text);
-        } else  if (std::get<1>(current_punc) == "END") {
-            std::string prefix_str = sub_texts[0] + std::get<0>(current_punc);
-            std::vector<std::string> suffix_str;
-            puncs.erase(puncs.begin());
-            sub_texts.erase(sub_texts.begin());
-            restore(puncs, sub_texts, suffix_str);
-            text.push_back(prefix_str + suffix_str[0]);
-        } else  if (sub_texts.size() == 1) {
-            sub_texts[0] += std::get<0>(current_punc);
-            puncs.erase(puncs.begin());
-            restore(puncs, sub_texts, text);
-        } else {
-            sub_texts[1] = sub_texts[0] + std::get<0>(current_punc) + sub_texts[1];
-            sub_texts.erase(sub_texts.begin());
-            puncs.erase(puncs.begin());
-            restore(puncs, sub_texts, text);
-        }
-    }
-}
-
-void removeChars(std::string str, const std::string& charsToRemove, std::string& output_str) {
-    output_str = str;
-    output_str.erase(std::remove_if(output_str.begin(), output_str.end(), [&](char c) {
-        return charsToRemove.find(c) != std::string::npos;
-    }), output_str.end());
-}
-
-
-void tts(std::map<std::string, int>& char2id, ov::CompiledModel& model,
-         const std::string& input_text, const std::string& output_wav_filename) {
-
-    std::vector<std::tuple<std::string, std::string>> puncs;
-    std::vector<std::string> sub_texts;
-    strip_to_restore(input_text, puncs, sub_texts);
-
-    std::vector<std::string> sub_phonemes;
-
-    for (auto s : sub_texts) {
-        const std::string* s_ptr = &s;
-        //std::cout << "processing:" << *s_ptr << std::endl;
-        std::string output = "";
-        espeak_Text2Phonemes((const void**)&s_ptr, 1, 19, &output);
-        if ((s[0] != ' ') && (output[0] == ' ')) {
-            output = output.substr(1,output.size()-1);
-        }
-        std::string output1 = "";
-        removeChars(output, "_", output1);
-        sub_phonemes.push_back(output1);
-    }
-
-
-    std::vector<std::string> restored_text;
-    restore(puncs, sub_phonemes, restored_text);
-
-    std::vector<int> ids;
-    decode_character(char2id, restored_text[0], ids);
-
-    std::vector<int> phoneme_ids;
-    insert_blank(ids, 178, phoneme_ids);
-
-    ov::InferRequest infer_request = model.create_infer_request();
-
-    ov::Tensor input_tensor_0 = infer_request.get_input_tensor(0);
-
-    input_tensor_0.set_shape({1, phoneme_ids.size()});
-    int64_t* x0 = input_tensor_0.data<int64_t>();
-    for (auto t : phoneme_ids) {
-        (*x0) = t;
-        x0 += 1;
-    }
-
-    ov::Tensor input_tensor_1 = infer_request.get_input_tensor(1);
-    input_tensor_1.set_shape({1});
-    int64_t* x1 = input_tensor_1.data<int64_t>();
-    *x1 = phoneme_ids.size();
-
-    ov::Tensor input_tensor_2 = infer_request.get_input_tensor(2);
-    //std::cout << "input_tensor_2.get_shape():" << input_tensor_2.get_shape() << std::endl;
-    float* x2 = input_tensor_2.data<float>();
-    std::vector<float> scales = {0.667, 1.0, 1.0};
-    //*z = scales[0];
-    //*(z+1) = scales[1];
-    //*(z+2) = scales[2];
-    for (auto s: scales) {
-        *x2 = s;
-        x2 += 1;
-    }
-
-    infer_request.infer();
-    const ov::Tensor& output_tensor1 = infer_request.get_output_tensor();
-    std::vector<float> wav_data;
-    for (auto yo = 0; yo < output_tensor1.get_size(); yo++) {
-        //std::cout << output_tensor1.data<float>()[yo] << ",";
-        wav_data.push_back(output_tensor1.data<float>()[yo]);
-    }
-
-    // 数据归一化
-    std::vector<float> wav_data_abs(wav_data.size());
-    std::transform(wav_data.begin(), wav_data.end(), wav_data_abs.begin(), [](float x) { return std::abs(x); });
-    auto max_it = std::max_element(wav_data_abs.begin(), wav_data_abs.end());
-    auto max_val = std::max(0.01f, *max_it);
-    std::transform(wav_data.begin(), wav_data.end(), wav_data.begin(), [max_val](float x) { return x * (32767 / max_val); });
-    std::vector<int16_t> wav_data_int(wav_data.size());
-    std::transform(wav_data.begin(), wav_data.end(), wav_data_int.begin(), [](float x) { return int16_t(x); });
-
-    // 写入数据到文件
-    SndfileHandle file(output_wav_filename, SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, 1, 22050);
-    file.write(wav_data_int.data(), wav_data_int.size());
-}
-
-async_simple::coro::Lazy<void> tts_http_server(std::map<std::string, int>& char2id, ov::CompiledModel& model) {
+async_simple::coro::Lazy<void> tts_http_server(std::map<std::string, int>& char2id, ov::CompiledModel& model, const status_type& init_status) {
     int max_thread_num = std::thread::hardware_concurrency();
     coro_http_server server(max_thread_num, 8080);
     std::cout << "max_thread_num:" << max_thread_num << std::endl;       
 
-    server.set_http_handler<GET, POST>("/TTS", [&char2id,&model](coro_http_request& req, coro_http_response& res) {
+    server.set_http_handler<GET, POST>("/TTS",
+        [&char2id,&model,&init_status](coro_http_request& req, coro_http_response& res) -> async_simple::coro::Lazy<void> {
+        //std::cout << static_cast<int>(init_status) << std::endl;
+        if (init_status != status_type::ok) {
+            res.set_status_and_content(init_status, "Failed to init TTS model!\n");
+            co_return;
+        }
         std::string input = "Hello,world!";
         auto req_queris = req.get_queries();
-        for (auto &[k, v] : req.get_queries()) {
-            std::cout << "k:" << k << ";v:" << v << std::endl;
-        }
+
         if (req_queris.contains("text")) {
             input = std::string(req.get_query_value("text"));
         }
+
+        // in curl, query value cannot contains white space, use '%20' instead.
+        replaceAll(input, "%20", " ");
         std::cout << "input:" << input << std::endl; 
 
-        std::string wav_filename = "output.wav";
+        std::string now = std::to_string(static_cast<int>(time(0)));
+        int input_number = 0;
+        get_random_number(input, input_number);
+        std::string wav_filename = now.append("_").append(std::to_string(input_number)).append(".wav");
 
-        tts(char2id, model, input, wav_filename);
+        status_type tts_ret_status;
+        tts(char2id, model, input, wav_filename, tts_ret_status);
+
+        if (tts_ret_status != status_type::ok) {
+            res.set_status_and_content(tts_ret_status, "Failed to run TTS!\n");
+            co_return;
+        }
 
         std::ifstream file(wav_filename, std::ios::binary);
         if (!file) {
-            std::cerr << "无法打开文件" << std::endl;
+            std::cerr << "Failed to open file:" << wav_filename << std::endl;
+            res.set_status_and_content(status_type::open_file_failed, "Failed to open wav file!\n");
+            co_return;
         }
     
         // 读取文件全部内容到字符串流
@@ -254,7 +83,8 @@ async_simple::coro::Lazy<void> tts_http_server(std::map<std::string, int>& char2
 
         // 使用读取到的数据进行操作
         res.set_status_and_content(status_type::ok, content);
-        
+
+        std::remove(wav_filename.c_str());
     });
 
     server.sync_start();
@@ -264,37 +94,36 @@ async_simple::coro::Lazy<void> tts_http_server(std::map<std::string, int>& char2
  * @brief Main with support Unicode paths, wide strings
  */
 int main(int argc, char* argv[]) {
-    try {
-        // -------- Get OpenVINO runtime version --------
-        std::cout << ov::get_openvino_version() << std::endl;
-        
-        
-        // -------- Parsing and validation of input arguments --------
-        if (argc != 2) {
-            std::cout << "Usage : " << argv[0] << " <path_to_model>"
-                       << std::endl;
-            return EXIT_FAILURE;
-        }
+    // -------- Get OpenVINO runtime version --------
+    // std::cout << ov::get_openvino_version() << std::endl;
 
-        const std::string model_path = argv[1];
-
-        ov::Core core;
-
-        std::cout << "Loading model files: " << model_path << std::endl;
-        std::shared_ptr<ov::Model> model = core.read_model(model_path);
-
-        ov::CompiledModel compiled_model = core.compile_model(model);
-
-        
-        // initialization
-        std::map<std::string, int> char2id = {{"_",0}, {";",1}, {":",2}, {",",3}, {".",4}, {"!",5}, {"?",6}, {"¡",7}, {"¿",8}, {"—",9}, {"…",10}, {"\"",11}, {"«",12}, {"»",13}, {"“",14}, {"”",15}, {" ",16}, {"A",17}, {"B",18}, {"C",19}, {"D",20}, {"E",21}, {"F",22}, {"G",23}, {"H",24}, {"I",25}, {"J",26}, {"K",27}, {"L",28}, {"M",29}, {"N",30}, {"O",31}, {"P",32}, {"Q",33}, {"R",34}, {"S",35}, {"T",36}, {"U",37}, {"V",38}, {"W",39}, {"X",40}, {"Y",41}, {"Z",42}, {"a",43}, {"b",44}, {"c",45}, {"d",46}, {"e",47}, {"f",48}, {"g",49}, {"h",50}, {"i",51}, {"j",52}, {"k",53}, {"l",54}, {"m",55}, {"n",56}, {"o",57}, {"p",58}, {"q",59}, {"r",60}, {"s",61}, {"t",62}, {"u",63}, {"v",64}, {"w",65}, {"x",66}, {"y",67}, {"z",68}, {"ɑ",69}, {"ɐ",70}, {"ɒ",71}, {"æ",72}, {"ɓ",73}, {"ʙ",74}, {"β",75}, {"ɔ",76}, {"ɕ",77}, {"ç",78}, {"ɗ",79}, {"ɖ",80}, {"ð",81}, {"ʤ",82}, {"ə",83}, {"ɘ",84}, {"ɚ",85}, {"ɛ",86}, {"ɜ",87}, {"ɝ",88}, {"ɞ",89}, {"ɟ",90}, {"ʄ",91}, {"ɡ",92}, {"ɠ",93}, {"ɢ",94}, {"ʛ",95}, {"ɦ",96}, {"ɧ",97}, {"ħ",98}, {"ɥ",99}, {"ʜ",100}, {"ɨ",101}, {"ɪ",102}, {"ʝ",103}, {"ɭ",104}, {"ɬ",105}, {"ɫ",106}, {"ɮ",107}, {"ʟ",108}, {"ɱ",109}, {"ɯ",110}, {"ɰ",111}, {"ŋ",112}, {"ɳ",113}, {"ɲ",114}, {"ɴ",115}, {"ø",116}, {"ɵ",117}, {"ɸ",118}, {"θ",119}, {"œ",120}, {"ɶ",121}, {"ʘ",122}, {"ɹ",123}, {"ɺ",124}, {"ɾ",125}, {"ɻ",126}, {"ʀ",127}, {"ʁ",128}, {"ɽ",129}, {"ʂ",130}, {"ʃ",131}, {"ʈ",132}, {"ʧ",133}, {"ʉ",134}, {"ʊ",135}, {"ʋ",136}, {"ⱱ",137}, {"ʌ",138}, {"ɣ",139}, {"ɤ",140}, {"ʍ",141}, {"χ",142}, {"ʎ",143}, {"ʏ",144}, {"ʑ",145}, {"ʐ",146}, {"ʒ",147}, {"ʔ",148}, {"ʡ",149}, {"ʕ",150}, {"ʢ",151}, {"ǀ",152}, {"ǁ",153}, {"ǂ",154}, {"ǃ",155}, {"ˈ",156}, {"ˌ",157}, {"ː",158}, {"ˑ",159}, {"ʼ",160}, {"ʴ",161}, {"ʰ",162}, {"ʱ",163}, {"ʲ",164}, {"ʷ",165}, {"ˠ",166}, {"ˤ",167}, {"˞",168}, {"↓",169}, {"↑",170}, {"→",171}, {"↗",172}, {"↘",173}, {"̩",175}, {"'",176}, {"ᵻ",177}, {"<BLNK>",178}};
-
-        async_simple::coro::syncAwait(tts_http_server(char2id, compiled_model));
-
-    } catch (const std::exception& ex) {
-            std::cerr << ex.what() << std::endl;
-            return EXIT_FAILURE;
+    // -------- Parsing and validation of input arguments --------
+    if (argc != 2) {
+        std::cout << "Usage : " << argv[0] << " <path_to_model>"
+                    << std::endl;
+        return EXIT_FAILURE;
     }
+    const std::string model_path = argv[1];
+    std::cout << "Loading model files: " << model_path << std::endl;
+        // initialization
+    std::map<std::string, int> char2id = {{"_",0}, {";",1}, {":",2}, {",",3}, {".",4}, {"!",5}, {"?",6}, {"¡",7}, {"¿",8}, {"—",9}, {"…",10}, {"\"",11}, {"«",12}, {"»",13}, {"“",14}, {"”",15}, {" ",16}, {"A",17}, {"B",18}, {"C",19}, {"D",20}, {"E",21}, {"F",22}, {"G",23}, {"H",24}, {"I",25}, {"J",26}, {"K",27}, {"L",28}, {"M",29}, {"N",30}, {"O",31}, {"P",32}, {"Q",33}, {"R",34}, {"S",35}, {"T",36}, {"U",37}, {"V",38}, {"W",39}, {"X",40}, {"Y",41}, {"Z",42}, {"a",43}, {"b",44}, {"c",45}, {"d",46}, {"e",47}, {"f",48}, {"g",49}, {"h",50}, {"i",51}, {"j",52}, {"k",53}, {"l",54}, {"m",55}, {"n",56}, {"o",57}, {"p",58}, {"q",59}, {"r",60}, {"s",61}, {"t",62}, {"u",63}, {"v",64}, {"w",65}, {"x",66}, {"y",67}, {"z",68}, {"ɑ",69}, {"ɐ",70}, {"ɒ",71}, {"æ",72}, {"ɓ",73}, {"ʙ",74}, {"β",75}, {"ɔ",76}, {"ɕ",77}, {"ç",78}, {"ɗ",79}, {"ɖ",80}, {"ð",81}, {"ʤ",82}, {"ə",83}, {"ɘ",84}, {"ɚ",85}, {"ɛ",86}, {"ɜ",87}, {"ɝ",88}, {"ɞ",89}, {"ɟ",90}, {"ʄ",91}, {"ɡ",92}, {"ɠ",93}, {"ɢ",94}, {"ʛ",95}, {"ɦ",96}, {"ɧ",97}, {"ħ",98}, {"ɥ",99}, {"ʜ",100}, {"ɨ",101}, {"ɪ",102}, {"ʝ",103}, {"ɭ",104}, {"ɬ",105}, {"ɫ",106}, {"ɮ",107}, {"ʟ",108}, {"ɱ",109}, {"ɯ",110}, {"ɰ",111}, {"ŋ",112}, {"ɳ",113}, {"ɲ",114}, {"ɴ",115}, {"ø",116}, {"ɵ",117}, {"ɸ",118}, {"θ",119}, {"œ",120}, {"ɶ",121}, {"ʘ",122}, {"ɹ",123}, {"ɺ",124}, {"ɾ",125}, {"ɻ",126}, {"ʀ",127}, {"ʁ",128}, {"ɽ",129}, {"ʂ",130}, {"ʃ",131}, {"ʈ",132}, {"ʧ",133}, {"ʉ",134}, {"ʊ",135}, {"ʋ",136}, {"ⱱ",137}, {"ʌ",138}, {"ɣ",139}, {"ɤ",140}, {"ʍ",141}, {"χ",142}, {"ʎ",143}, {"ʏ",144}, {"ʑ",145}, {"ʐ",146}, {"ʒ",147}, {"ʔ",148}, {"ʡ",149}, {"ʕ",150}, {"ʢ",151}, {"ǀ",152}, {"ǁ",153}, {"ǂ",154}, {"ǃ",155}, {"ˈ",156}, {"ˌ",157}, {"ː",158}, {"ˑ",159}, {"ʼ",160}, {"ʴ",161}, {"ʰ",162}, {"ʱ",163}, {"ʲ",164}, {"ʷ",165}, {"ˠ",166}, {"ˤ",167}, {"˞",168}, {"↓",169}, {"↑",170}, {"→",171}, {"↗",172}, {"↘",173}, {"̩",175}, {"'",176}, {"ᵻ",177}, {"<BLNK>",178}};
+    ov::Core core;
+    std::shared_ptr<ov::Model> model;
+    ov::CompiledModel compiled_model;
+
+    status_type init_status = status_type::ok;
+    try {
+        model = core.read_model(model_path);
+        compiled_model = core.compile_model(model);
+    } catch (const std::exception& ex) {
+        std::cerr << "Failed to init TTS model!\n";
+        std::cerr << ex.what() << std::endl;
+        init_status = status_type::init_tts_model_failed;
+        //std::cout << static_cast<int>(init_status) << std::endl;
+        //std::cout << static_cast<int>(status_type::init_tts_model_failed) << std::endl;
+    }
+
+    async_simple::coro::syncAwait(tts_http_server(char2id, compiled_model, init_status));
 
     std::cout << "Finished successfully!\n";
     return EXIT_SUCCESS;
---
+++ launch/cpp/openvino/utils.hpp
@@ -0,0 +1,243 @@
+#include <iterator>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <sndfile.hh>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+#include <fstream>
+#include <iostream>
+
+
+#include "boost/regex.hpp"
+#include "speak_lib.h"
+#include "openvino/openvino.hpp"
+#include "cinatra.hpp"
+#include "cinatra/metric_conf.hpp"
+using namespace cinatra;
+
+enum class tts_status {
+  init,
+  ok = 200,
+  init_failed = 300,
+  inference_failed = 400
+};
+
+void decode_character(std::map<std::string, int>& char2id_map,
+                    const std::string& input_str,
+                    std::vector<int>& ids) {
+    size_t idx = 0;
+    while (idx < input_str.size()) {
+		if ((input_str[idx] & 0x80) != 0){
+            std::string ch = input_str.substr(idx, 2);
+            ids.push_back(char2id_map[ch]);
+            idx += 2;
+        } else {
+            std::string ch = input_str.substr(idx, 1);
+            ids.push_back(char2id_map[ch]);
+            idx += 1;
+        }
+
+    }      
+}
+
+void  insert_blank(const std::vector<int>& input_ids,
+                 const int& blank_id,
+                 std::vector<int>& output_ids) {
+    output_ids.push_back(blank_id);
+
+    for(auto id : input_ids) {
+        output_ids.push_back(id);
+        output_ids.push_back(blank_id);
+    }                
+}
+
+
+int strip_to_restore(const std::string& text,
+                    std::vector<std::tuple<std::string, std::string>>& puncs,
+                    std::vector<std::string>& sub_texts) {
+    //std::cout << "get text:" << text << std::endl;
+    boost::regex reg("[«»,.!]+");
+    boost::sregex_iterator it(text.begin(),text.end(),reg);
+    boost::sregex_iterator begin, end;
+
+    while (it!=end) {
+        std::string punc_label = (it++)->str();
+        std::string position = "MIDDLE";
+        if (text.substr(0, punc_label.size()) == punc_label)
+            position = "BEGIN";
+        else if (text.substr(text.size()-punc_label.size(), text.size()) == punc_label) {
+            position = "END";
+        }
+            
+        puncs.push_back(std::tuple<std::string,std::string>(punc_label, position));
+    }
+
+    boost::sregex_token_iterator tit(text.begin(),text.end(),reg,-1);
+    boost::sregex_token_iterator tend;
+    while (tit!=tend) {
+        std::string t = *tit++;
+        if (t != "")
+            sub_texts.push_back(t);
+    }
+}
+
+
+int restore(std::vector<std::tuple<std::string, std::string>>& puncs,
+            std::vector<std::string>& sub_texts,
+            std::vector<std::string>& text) {
+    if (puncs.size() <= 0) {
+        std::string text_s = "";
+        for (auto s : sub_texts) {
+            text_s += s;
+        }
+        text.push_back(text_s);
+        //std::cout << text[0] << std::endl;
+    } else if (sub_texts.size() <= 0) {
+        std::string punc_str = "";
+        for (auto p : puncs) {
+            punc_str += std::get<0>(p);
+        }
+        text.push_back(punc_str);
+        //std::cout << text[0] << std::endl;
+    } else {
+
+        std::tuple<std::string, std::string> current_punc = puncs[0];
+        if (std::get<1>(current_punc) == "BEGIN") {
+            std::string prefix_str = std::get<0>(current_punc) + sub_texts[0];
+            puncs.erase(puncs.begin());
+            sub_texts[0] = prefix_str;
+            restore(puncs, sub_texts, text);
+        } else  if (std::get<1>(current_punc) == "END") {
+            std::string prefix_str = sub_texts[0] + std::get<0>(current_punc);
+            std::vector<std::string> suffix_str;
+            puncs.erase(puncs.begin());
+            sub_texts.erase(sub_texts.begin());
+            restore(puncs, sub_texts, suffix_str);
+            text.push_back(prefix_str + suffix_str[0]);
+        } else  if (sub_texts.size() == 1) {
+            sub_texts[0] += std::get<0>(current_punc);
+            puncs.erase(puncs.begin());
+            restore(puncs, sub_texts, text);
+        } else {
+            sub_texts[1] = sub_texts[0] + std::get<0>(current_punc) + sub_texts[1];
+            sub_texts.erase(sub_texts.begin());
+            puncs.erase(puncs.begin());
+            restore(puncs, sub_texts, text);
+        }
+    }
+}
+
+void removeChars(std::string str, const std::string& charsToRemove, std::string& output_str) {
+    output_str = str;
+    output_str.erase(std::remove_if(output_str.begin(), output_str.end(), [&](char c) {
+        return charsToRemove.find(c) != std::string::npos;
+    }), output_str.end());
+}
+
+
+void tts(std::map<std::string, int>& char2id, ov::CompiledModel& model,
+         const std::string& input_text, const std::string& output_wav_filename,
+         status_type& return_status) {
+    try{
+        std::vector<std::tuple<std::string, std::string>> puncs;
+        std::vector<std::string> sub_texts;
+        strip_to_restore(input_text, puncs, sub_texts);
+
+        std::vector<std::string> sub_phonemes;
+
+        for (auto s : sub_texts) {
+            const std::string* s_ptr = &s;
+            //std::cout << "processing:" << *s_ptr << std::endl;
+            std::string output = "";
+            espeak_Text2Phonemes((const void**)&s_ptr, 1, 19, &output);
+            if ((s[0] != ' ') && (output[0] == ' ')) {
+                output = output.substr(1,output.size()-1);
+            }
+            std::string output1 = "";
+            removeChars(output, "_", output1);
+            sub_phonemes.push_back(output1);
+        }
+
+
+        std::vector<std::string> restored_text;
+        restore(puncs, sub_phonemes, restored_text);
+
+        std::vector<int> ids;
+        decode_character(char2id, restored_text[0], ids);
+
+        std::vector<int> phoneme_ids;
+        insert_blank(ids, 178, phoneme_ids);
+
+        ov::InferRequest infer_request = model.create_infer_request();
+
+        ov::Tensor input_tensor_0 = infer_request.get_input_tensor(0);
+
+        input_tensor_0.set_shape({1, phoneme_ids.size()});
+        int64_t* x0 = input_tensor_0.data<int64_t>();
+        for (auto t : phoneme_ids) {
+            (*x0) = t;
+            x0 += 1;
+        }
+
+        ov::Tensor input_tensor_1 = infer_request.get_input_tensor(1);
+        input_tensor_1.set_shape({1});
+        int64_t* x1 = input_tensor_1.data<int64_t>();
+        *x1 = phoneme_ids.size();
+
+        ov::Tensor input_tensor_2 = infer_request.get_input_tensor(2);
+        //std::cout << "input_tensor_2.get_shape():" << input_tensor_2.get_shape() << std::endl;
+        float* x2 = input_tensor_2.data<float>();
+        std::vector<float> scales = {0.667, 1.0, 1.0};
+        //*z = scales[0];
+        //*(z+1) = scales[1];
+        //*(z+2) = scales[2];
+        for (auto s: scales) {
+            *x2 = s;
+            x2 += 1;
+        }
+
+        infer_request.infer();
+        const ov::Tensor& output_tensor1 = infer_request.get_output_tensor();
+        std::vector<float> wav_data;
+        for (auto yo = 0; yo < output_tensor1.get_size(); yo++) {
+            //std::cout << output_tensor1.data<float>()[yo] << ",";
+            wav_data.push_back(output_tensor1.data<float>()[yo]);
+        }
+
+        // 数据归一化
+        std::vector<float> wav_data_abs(wav_data.size());
+        std::transform(wav_data.begin(), wav_data.end(), wav_data_abs.begin(), [](float x) { return std::abs(x); });
+        auto max_it = std::max_element(wav_data_abs.begin(), wav_data_abs.end());
+        auto max_val = std::max(0.01f, *max_it);
+        std::transform(wav_data.begin(), wav_data.end(), wav_data.begin(), [max_val](float x) { return x * (32767 / max_val); });
+        std::vector<int16_t> wav_data_int(wav_data.size());
+        std::transform(wav_data.begin(), wav_data.end(), wav_data_int.begin(), [](float x) { return int16_t(x); });
+
+        // 写入数据到文件
+        SndfileHandle file(output_wav_filename, SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, 1, 22050);
+        file.write(wav_data_int.data(), wav_data_int.size());
+    } catch(const std::exception& ex) {
+        std::cerr << "Failed to run TTS inference\n";
+        std::cerr << ex.what() << std::endl;
+        return_status = status_type::tts_inference_failed;
+    }
+    return_status = status_type::ok;
+}
+
+void get_random_number(const std::string& str, int& random_int) {
+    std::hash<std::string> hash_fn;
+    std::mt19937 rng(hash_fn(str));  // 初始化随机数生成器
+    std::uniform_int_distribution<int> distr(0, std::numeric_limits<int>::max());
+    random_int = distr(rng);  // 生成随机数
+}
+
+void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while ((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // 防止无限循环，如果`to`包含`from`
+    }
+}
